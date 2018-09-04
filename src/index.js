45b7e4570d5c9b685b29c51082756bd2b38d6532
Shiyu Wang <shiyu3169@gmail.com>
2018-09-04 10:28:26 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { createStore } from 'redux';
 import { Provider, connect } from 'react-redux';
 
-const SomeComponent = ({dispatch}) => (
-    <button onClick={e => {
-        dispatch({type: 'some action'})
-    }}>Some button</button>
+// Component
+const SomeComponent = ({someComponentProperty, dispatch}) => (
+    <div>
+        <h2>Some Component</h2>
+        <button onClick={
+            () => dispatch({type: 'ACTION1'})
+        }>Action1</button>
+        <button onClick={
+            () => dispatch({type: 'ACTION2'})
+        }>Action2</button>
+        <h3>{someComponentProperty}</h3>
+    </div>
 )
 
-const SomeContainer = connect()(SomeComponent)
-
-const someReducer = (state = {someDefaultProperty: 'some state'}, action) => {
-    switch (action.type) {
-        case 'some action': 
-            alert('Some action was dispatched')
-            return {someNewAttribute: 'some new state'}
-        defualt: 
-            return state
+// Mapper
+function someStateToPropsMapper(state) {
+    return {
+        someComponentProperty: state.someStateAttribute
     }
 }
 
+// Container
+const SomeContainer = connect(someStateToPropsMapper)(SomeComponent)
+
+// Reducer
+const someReducer = (state = {someDefaultProperty: 'some state'}, action) => {
+    switch (action.type) {
+        case 'ACTION1': 
+            // alert('ACTION1');
+            return {someStateAttribute: 'some new state 1'}
+        case 'ACTION2': 
+            // alert('ACTION2');
+            return {someStateAttribute: 'some new state 2'}
+        default: 
+            return state;
+    }
+}
+
+// Store
 const someStore = createStore(someReducer)
 
+// Render
 ReactDOM.render(
     <Provider store={someStore}>    
         <SomeContainer/>
