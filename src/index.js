51519f43bed3edb1b52eb131f0a4a592e39bc765
Shiyu Wang <shiyu3169@gmail.com>
2018-09-04 10:12:24 -0400

    store

--- src/index.js
+++ src/index.js
@@ -7,12 +7,26 @@ import { Provider, connect } from 'react-redux';
 const SomeComponent = ({dispatch}) => (
     <button onClick={e => {
         dispatch({type: 'some action'})
-    }
-
-    }>Some button</button>
+    }}>Some button</button>
 )
 
+const SomeContainer = connect()(SomeComponent)
+
+const someReducer = (state = {someDefaultProperty: 'some state'}, action) => {
+    switch (action.type) {
+        case 'some action': 
+            alert('Some action was dispatched')
+            return {someNewAttribute: 'some new state'}
+        defualt: 
+            return state
+    }
+}
+
+const someStore = createStore(someReducer)
+
 ReactDOM.render(
-    <SomeComponent/>,
+    <Provider store={someStore}>    
+        <SomeContainer/>
+    </Provider>,
     document.getElementById('root')
 )
